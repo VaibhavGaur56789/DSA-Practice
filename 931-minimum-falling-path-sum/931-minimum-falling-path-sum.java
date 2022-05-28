51b31952e1338fc8c746a7ee764ac42129c8701e
Vaibhav Gaur <VaibhavGaur56789@gmail.com>
2022-05-28 21:28:20 +0530

    Time: 11 ms (12.87%), Space: 49.3 MB (7.97%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -1,29 +1,33 @@
 class Solution {
-    //int dp[101][101];
     public int minFallingPathSum(int[][] matrix) {
         int[][] dp = new int[101][101];
         int n = matrix.length;
-        int m = matrix[0].length;
+        int m = matrix.length;
         
-        for(int i=0; i<m; i++) {
-            dp[0][i] = matrix[0][i]; //Fill first row 
-        }
+        for(int i=0; i<m; i++) 
+            dp[0][i] = matrix[0][i];
         
         for(int i=1; i<n; i++) {
             for(int j=0; j<m; j++) {
                 int mini = 10005;
-                if(i-1 >= 0)
-                    mini = Math.min(mini, dp[i-1][j]);
-                if(i-1 >= 0 && j-1 >= 0)
-                    mini = Math.min(mini, dp[i-1][j-1]);
-                if(i-1 >= 0 && j+1 < m)
-                    mini = Math.min(mini, dp[i-1][j+1]);
+                int up = matrix[i][j] + dp[i-1][j];
                 
-                dp[i][j] = mini + matrix[i][j];
+                int leftD = matrix[i][j];
+                if(j-1 >= 0)
+                    leftD += dp[i-1][j-1];
+                else leftD += mini;
+                
+                int rightD = matrix[i][j];
+                if(j+1 < m)
+                    rightD += dp[i-1][j+1];
+                else rightD += mini;
+                
+                dp[i][j] = Math.min(up, Math.min(leftD, rightD));
             }
         }
+        
         int ans = dp[n-1][0];
-        for(int i=1; i<m; i++)
+        for(int i=0; i<m; i++)
             ans = Math.min(ans, dp[n-1][i]);
         return ans;
     }
