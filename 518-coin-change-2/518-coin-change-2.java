4bd424dae540120f9db54168b099f5a2ee8ed9df
Vaibhav Gaur <VaibhavGaur56789@gmail.com>
2022-06-05 10:45:56 +0530

    Time: 15 ms (31.89%), Space: 50.7 MB (37.20%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.java
@@ -0,0 +1,40 @@
+class Solution {
+    
+    //Memoization
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        
+        int[][] dp = new int[n][amount+1];
+        for(int[] row: dp) {
+            Arrays.fill(row, -1);
+        }
+        
+        return changeUtil(coins, n-1, amount, dp);
+    }
+    
+    public int changeUtil(int[] coins, int ind, int amount, int[][] dp) {
+        if(ind == 0) {
+            if(amount % coins[0] == 0)
+                return 1;
+            else 
+                return 0;
+        }
+        
+        if(dp[ind][amount] != -1)
+            return dp[ind][amount];
+        
+        int notTaken = changeUtil(coins, ind-1, amount, dp);
+        int taken = 0;
+        if(coins[ind] <= amount) {
+            taken = changeUtil(coins, ind, amount - coins[ind], dp);
+        }
+        
+        return dp[ind][amount] = taken + notTaken; 
+    }
+}
+
+
+//     public int change(int amount, int[] coins) {
+            
+//     }
+// }
