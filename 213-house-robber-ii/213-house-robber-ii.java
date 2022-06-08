92dd696f6eb781254c3fd054cd358aa5717f71ae
Vaibhav Gaur <VaibhavGaur56789@gmail.com>
2022-06-08 21:20:07 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (25.49%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1)
+            return nums[0];
+        
+        int[] nums1 = new int[n];
+        int[] nums2 = new int[n];
+        
+        for(int i=0; i<n; i++) {
+            if(i != 0)
+                nums1[i] = nums[i];
+            if(i != n-1)
+                nums2[i] = nums[i];
+        }
+        int ans1 = solve(nums1);
+        int ans2 = solve(nums2);
+        
+        return Math.max(ans1, ans2);
+    }
+    
+    public static int solve(int[] nums) {
+        int n = nums.length;
+        int prev = nums[0];
+        int prev2 = 0;
+        
+        for(int i=1; i<n; i++) {
+             int pick = nums[i];
+            if(i > 1)
+                pick += prev2;
+            int notPick = 0 + prev;
+            
+            int curr = Math.max(pick, notPick);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+}
