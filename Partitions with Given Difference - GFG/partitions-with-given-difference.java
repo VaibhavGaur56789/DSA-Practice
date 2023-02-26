59b9895730350d957353f524dea4b879950366cf
Vaibhav Gaur <VaibhavGaur56789@gmail.com>
2023-02-27 02:11:20 +0530

    Added solution - LeetHub

---
+++ Partitions with Given Difference - GFG/partitions-with-given-difference.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG{
+    public static void main(String args[]) throws IOException{
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int d=sc.nextInt();
+            
+            int []arr=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+            
+            Solution obj=new Solution();
+            int res=obj.countPartitions(n, d, arr);
+            System.out.println(res);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+//Back-end complete function Template for Java
+
+class Solution{
+
+    static int mod = (int)(Math.pow(10,9) + 7);
+    
+    public int countPartitions(int n, int d, int arr[]){
+        // Code here
+     int totSum = 0;
+    for(int i=0; i<arr.length;i++){
+        totSum += arr[i];
+    }
+    
+    //Checking for edge cases
+    if(totSum-d<0) return 0;
+    if((totSum-d)%2==1) return 0;
+    
+    int s2 = (totSum-d)/2;
+    
+    int dp[][] = new int[n][s2+1];
+    
+    for(int row[]: dp)
+    Arrays.fill(row,-1);
+    
+    return countPartitionsUtil(n-1,s2,arr,dp);
+    }
+    
+   static int countPartitionsUtil(int ind, int target,int[] arr, int[][] dp){
+
+     if(ind == 0){
+        if(target==0 && arr[0]==0)
+            return 2;
+        if(target==0 || target == arr[0])
+            return 1;
+        return 0;
+    }
+    
+    if(dp[ind][target]!=-1)
+        return dp[ind][target];
+        
+    int notTaken = countPartitionsUtil(ind-1,target,arr,dp);
+    
+    int taken = 0;
+    if(arr[ind]<=target)
+        taken = countPartitionsUtil(ind-1,target-arr[ind],arr,dp);
+        
+    return dp[ind][target]= (notTaken + taken)%mod;
+}
+}
+    
+    
